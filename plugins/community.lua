35d49997d151c778c9fff0363541766ff59ea8da
Richie-Z <zakariarichie@yahoo.com>
2023-07-02 18:55:35 +0700

    enhance(plugins): use custom leap-nvim config for enable surround when in visual mode

--- plugins/community.lua
+++ plugins/community.lua
@@ -17,7 +17,6 @@ return {
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.tailwindcss" },
 
-  { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.flit-nvim" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
