3cf3e1fbf7a3c3de183968cca31e219b2d9fba63
Richie-Z <zakariarichie@yahoo.com>
2023-07-02 18:30:19 +0700

    feat(plugins): add some productivity astocommunity plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,7 @@ return {
     "loctvl842/monokai-pro.nvim",
     config = function()
       require("monokai-pro").setup {
+        transparent_background = true,
         filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
       }
     end,
@@ -12,4 +13,15 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.tailwindcss" },
+
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flit-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.nvim-surround" },
+
+  { import = "astrocommunity.color.tint-nvim" },
+  { import = "astrocommunity.color.twilight-nvim" },
 }
