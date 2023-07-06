db2859fecf4ac4431b9402320822ea09f65eb93e
Richie-Z <zakariarichie@yahoo.com>
2023-07-07 00:24:48 +0700

    feat(plugins): add php-cs-fixer

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,6 +1,7 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
+    config.ensure_installed = require("astronvim.utils").list_insert_unique(config.ensure_installed, { "php-cs-fixer" })
     -- config variable is the default configuration table for the setup function call
     -- local null_ls = require "null-ls"
 
